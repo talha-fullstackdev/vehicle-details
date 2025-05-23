f6fb38d0374ee92a2e81e03a5ab24693380497ef
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-23 13:11:07 +0500

    made api's for the project

---
+++ src/app/api/get-vehicle/route.jsx
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { connectionString } from "@/db/db_connection";
+import { VehicleModel } from "@/db/vehicleModel";
+import { NextResponse } from "next/server";
+export const GET = async(req,res)=>{
+    try{
+        await mongoose.connect(connectionString)
+        const data = await VehicleModel.find()
+        return NextResponse.json(
+            data
+        )
+
+    }catch(err){
+        return NextResponse.json(
+            {msg:"server side error"},{status:500}
+        )
+    }
+}
