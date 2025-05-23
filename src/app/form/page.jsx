fefeacfaebf8e5823a58eed1000f52c582e0a6f0
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-23 17:58:52 +0500

    integrated api with frontend and posted data succesfully to backend

--- src/app/form/page.jsx
+++ src/app/form/page.jsx
@@ -1,9 +1,79 @@
+"use client";
 import React from "react";
 import Form from "../components/Form";
+import { useRef } from "react";
+
 const Page = () => {
+  const vccNoRef = useRef();
+  const vccStatusRef = useRef();
+  const vccDateRef = useRef();
+  const chassisNoRef = useRef();
+  const engineNumberRef = useRef();
+  const yearBuiltRef = useRef();
+  const vehicleDriveRef = useRef();
+  const originRef = useRef();
+  const engineCapacityRef = useRef();
+  const carriageCapacityRef = useRef();
+  const passengerCapacityRef = useRef();
+  const vehicleModelRef = useRef();
+  const brandNameRef = useRef();
+  const vehicleTypeRef = useRef();
+  const vehicleColorRef = useRef();
+  const specStandardRef = useRef();
+  const declarationNoRef = useRef();
+  const declarationDateRef = useRef();
+  const ownerCodeRef = useRef();
+  const ownerNameRef = useRef();
+  const printRemarksRef = useRef();
+  const clearInputs = () => {
+    vccNoRef.current.value = "";
+    vccStatusRef.current.value = "";
+    vccDateRef.current.value = "";
+    chassisNoRef.current.value = "";
+    engineNumberRef.current.value = "";
+    yearBuiltRef.current.value = "";
+    vehicleDriveRef.current.value = "";
+    originRef.current.value = "";
+    engineCapacityRef.current.value = "";
+    carriageCapacityRef.current.value = "";
+    passengerCapacityRef.current.value = "";
+    vehicleModelRef.current.value = "";
+    brandNameRef.current.value = "";
+    vehicleTypeRef.current.value = "";
+    vehicleColorRef.current.value = "";
+    specStandardRef.current.value = "";
+    declarationNoRef.current.value = "";
+    declarationDateRef.current.value = "";
+    ownerCodeRef.current.value = "";
+    ownerNameRef.current.value = "";
+    printRemarksRef.current.value = "";
+  };
+  const data = {
+    vccNoRef,
+    vccStatusRef,
+    vccDateRef,
+    chassisNoRef,
+    engineNumberRef,
+    yearBuiltRef,
+    vehicleDriveRef,
+    originRef,
+    engineCapacityRef,
+    carriageCapacityRef,
+    passengerCapacityRef,
+    vehicleModelRef,
+    brandNameRef,
+    vehicleTypeRef,
+    vehicleColorRef,
+    specStandardRef,
+    declarationNoRef,
+    declarationDateRef,
+    ownerCodeRef,
+    ownerNameRef,
+    printRemarksRef,
+  };
   return (
     <>
-       <Form/>
+      <Form data={data} clearInputs={clearInputs} />
     </>
   );
 };
