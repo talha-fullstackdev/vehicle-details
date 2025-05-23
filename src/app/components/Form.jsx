7e15b77fa12318fdcd9251ad362cf3491e635951
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-23 17:57:43 +0500

    correct the eror pass json.stringyfy(data)

--- src/app/components/Form.jsx
+++ src/app/components/Form.jsx
@@ -1,53 +1,31 @@
-"use client";
 import React from "react";
-import { useRef } from "react";
-const Form = () => {
-  const vccNoRef = useRef();
-  const vccStatusRef = useRef();
-  const vccDateRef = useRef();
-  const chassisNoRef = useRef();
-  const engineNumberRef = useRef();
-  const yearBuiltRef = useRef();
-  const vehicleDriveRef = useRef();
-  const originRef = useRef();
-  const engineCapacityRef = useRef();
-  const carriageCapacityRef = useRef();
-  const passengerCapacityRef = useRef();
-  const vehicleModelRef = useRef();
-  const brandNameRef = useRef();
-  const vehicleTypeRef = useRef();
-  const vehicleColorRef = useRef();
-  const specStandardRef = useRef();
-  const declarationNoRef = useRef();
-  const declarationDateRef = useRef();
-  const ownerCodeRef = useRef();
-  const ownerNameRef = useRef();
-  const printRemarksRef = useRef();
-  const clearInputs = () => {
-    vccNoRef.current.value = "";
-    vccStatusRef.current.value = "";
-    vccDateRef.current.value = "";
-    chassisNoRef.current.value = "";
-    engineNumberRef.current.value = "";
-    yearBuiltRef.current.value = "";
-    vehicleDriveRef.current.value = "";
-    originRef.current.value = "";
-    engineCapacityRef.current.value = "";
-    carriageCapacityRef.current.value = "";
-    passengerCapacityRef.current.value = "";
-    vehicleModelRef.current.value = "";
-    brandNameRef.current.value = "";
-    vehicleTypeRef.current.value = "";
-    vehicleColorRef.current.value = "";
-    specStandardRef.current.value = "";
-    declarationNoRef.current.value = "";
-    declarationDateRef.current.value = "";
-    ownerCodeRef.current.value = "";
-    ownerNameRef.current.value = "";
-    printRemarksRef.current.value = "";
-  };
+const Form = ({ data, clearInputs }) => {
+  const {
+    vccNoRef,
+    vccStatusRef,
+    vccDateRef,
+    chassisNoRef,
+    engineNumberRef,
+    yearBuiltRef,
+    vehicleDriveRef,
+    originRef,
+    engineCapacityRef,
+    carriageCapacityRef,
+    passengerCapacityRef,
+    vehicleModelRef,
+    brandNameRef,
+    vehicleTypeRef,
+    vehicleColorRef,
+    specStandardRef,
+    declarationNoRef,
+    declarationDateRef,
+    ownerCodeRef,
+    ownerNameRef,
+    printRemarksRef,
+  } = data;
+
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const data = {
       vccNo: vccNoRef.current.value,
       vccStatus: vccStatusRef.current.value,
@@ -72,7 +50,21 @@ const Form = () => {
       printRemarks: printRemarksRef.current.value,
     };
     console.log(data);
-    clearInputs()
+    try {
+      let result = await fetch("http://localhost:3000/api/post-vehicle", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      result = await result.json();
+      if (result.succces) {
+        alert("successfully uploaded");
+         clearInputs();
+      }
+      
+    } catch (err) {
+      console.error(err, "server side error");
+      alert("failed to upload");
+    }
   };
 
   return (
