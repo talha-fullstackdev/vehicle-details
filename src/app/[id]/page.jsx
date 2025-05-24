af4980b0c5005f3ad95ba530199ec3e5fc10461c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-24 13:18:29 +0500

    made a dynamic page route to get individual car data

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,11 +1,12 @@
 // "use client"
 import React from 'react'
+import GetData from '../components/GetData'
 const page = async ({params}) => {
   
     const {id} =  await params
    
   return (
-    <h1>this is new home page of id {id}</h1>
+    <GetData id={id}/>
   )
 }
 
