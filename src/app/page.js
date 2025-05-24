0db7df960d43c4b7e0c7144e799c21ff22bdb089
talha-fullstackdev <talhafullstack786@gmail.com>
2025-05-24 15:08:23 +0500

    remove extra code from home page

--- src/app/page.js
+++ src/app/page.js
@@ -28,25 +28,6 @@ export default function Home() {
         {vehicleData.map((vehicle) => (
           <div key={vehicle._id} className="card">
             <h2>{vehicle.brandName} {vehicle.vehicleModel}</h2>
-            {/* <p><strong>VCC No:</strong> {vehicle.vccNo}</p>
-            <p><strong>Status:</strong> {vehicle.vccStatus}</p>
-            <p><strong>VCC Date:</strong> {new Date(vehicle.vccDate).toLocaleDateString()}</p>
-            <p><strong>Chassis No:</strong> {vehicle.chassisNo}</p>
-            <p><strong>Engine Number:</strong> {vehicle.engineNumber}</p>
-            <p><strong>Year Built:</strong> {vehicle.yearBuilt}</p>
-            <p><strong>Drive:</strong> {vehicle.vehicleDrive}</p>
-            <p><strong>Origin:</strong> {vehicle.origin}</p>
-            <p><strong>Engine Capacity:</strong> {vehicle.engineCapacity}</p>
-            <p><strong>Carriage Capacity:</strong> {vehicle.carriageCapacity}</p>
-            <p><strong>Passenger Capacity:</strong> {vehicle.passengerCapacity}</p>
-            <p><strong>Vehicle Type:</strong> {vehicle.vehicleType}</p>
-            <p><strong>Color:</strong> {vehicle.vehicleColor}</p>
-            <p><strong>Standard:</strong> {vehicle.specStandard}</p>
-            <p><strong>Declaration No:</strong> {vehicle.declarationNo}</p>
-            <p><strong>Declaration Date:</strong> {new Date(vehicle.declarationDate).toLocaleDateString()}</p>
-            <p><strong>Owner Code:</strong> {vehicle.ownerCode}</p>
-            <p><strong>Owner Name:</strong> {vehicle.ownerName}</p>
-            <p><strong>Remarks:</strong> {vehicle.printRemarks}</p> */}
             <div className="QR_code" style={{ marginTop: "10px" }}>
               <Link href={`/${vehicle._id}`}>
               <QRCode 
